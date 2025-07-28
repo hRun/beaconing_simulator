6a7d7442447e218a26fc9a0958bb486a406b9054
hRun <info@kuessner-consulting.de>
2025-07-28 16:52:48 +0200

    added mass simulation script

--- beaconing_simulation.py
+++ beaconing_simulation.py
@@ -4,13 +4,10 @@ a script to simulate malware beaconing in a slightly more sophistocated way than
 features:
     - simulate the beacon halting for a period of time due to the compromised device being switched off / asleep / ...
     - simulate the beacon receiving a command from the c2 server: multiple larger responses and larger requests to immitate commands and execution results going back and forth for a few minutes. the beacon then slows down for a while assuming the operator works on the results
-    - simulate the beacon exfiltrating data: a very large request followed by some silence
+    - simulate the beacon exfiltrating data: a very large request followed by some silence 
     - simulate parallel user activity as background noise
     - run the simulation in a "log only" mode, not making any actual network requests, but writing a log file which should look similar to what your proxy/etc. would produce
-    - jitter, intervals and maximum number of requests obviously
-    - TODO support of multiple protocols
-    - TODO support of more http methods
-    - TODO support of round robin c2 servers
+    - jitter, intervals and maximum number of requests obviously, round robin, multiple protocols, etc.
 """
 
 # TODO simulation of c2 and exfil requires a server-side process to control response sizes. one endpoint for regular small responses, one endpoint for big responses (tool transfer / sending commands) -> size should change after each request, one endpoint for random responses (noise)
@@ -31,7 +28,7 @@ from websocket_beacon import WebsocketBeacon
 
 
 
-def simulate_beaconing():
+def simulate_beaconing(beacon):
     """
     main function to control beaconing based on the provided commandline arguments
 
@@ -107,7 +104,7 @@ def simulate_beaconing():
     beacon.done = True
 
 
-def make_background_noise():
+def make_background_noise(beacon):
     """
     make one or multiple requests simulating normal user activity while a beacon is going
     """
@@ -130,53 +127,50 @@ def make_background_noise():
 
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("destination", help="beaconing destination. i.e. the c2 server (fqdn or ip)", type=str)
-parser.add_argument("interval", help="default beaconing interval (in seconds). default is 30 seconds", type=int, default=30)
-parser.add_argument("max_requests", help="end the simulation after X requests. default is 720 requests, equating to ~6 hours with a 30 second interval", type=int, default=720)
-parser.add_argument("--absence", help="make a significant pause of X minutes during the test to simulate the device being offline/sleeping/... default is no absence", type=int, default=0)
-parser.add_argument("--active_c2_ratio", help="the percentage of requests which should simulate active usage of the c2 channel. i.e. command and result exchange. default is between 0.1 and 3 percent", type=float, default=0.0)
-parser.add_argument("--data_jitter", help="if log_only is set, add random jitter to the request and response sizes (in percent). default is 11 percent", type=int, default=11)
-parser.add_argument("--exfil_chunking", help="use chunking when exfiltrating data. i.e. send many small requests with data contained in unique headers or uris instead of one large one (in protocols where applicable). default is to not use chunking", type=str, choices=['NONE', 'HEADER', 'URI'], default='NONE')
-parser.add_argument("--jitter", help="add random jitter to the time intervals between the beaconing requests (in percent of intervals). default is 17 percent", type=int, default=17)
-parser.add_argument("--log_only", help="only write log events as they would be expected from the simulation, don't actually dispatch requests. default is to make real requests", action="store_true", default=False)
-parser.add_argument("--no_c2", help="don't simulate the beacon receiving instructions from the c2 server (i.e. some larger responses, followed by larger requests, followed by temporary slower beaconing). default is to simulate c2 activity", action="store_true", default=False)
-parser.add_argument("--no_exfil", help="don't simulate the beacon exfiltrating data (similar to c2, but with significantly larger outflow). default is to simulate data exfiltration", action="store_true", default=False)
-parser.add_argument("--no_noise", help="don't make semi-random, semi-realistic non-beaconing requests in the background to add noise (as user activity would). default is to make background noise", action="store_true", default=False)
-parser.add_argument("--protocol", help="network protocol to use for beaconing communication. default is http", type=str, choices=['HTTP', 'HTTPS', 'SOCKS', 'WEBSOCKET'], default='HTTP')  # TODO choices=['DNS', 'TCP', 'UDP', '...']
-parser.add_argument("--reduce_interval_after_c2", help="reduce the polling interval after an active session, simulating how higher stealth could be achieved while the operator works on obtained data", action="store_true", default=False)
-parser.add_argument("--request_method", help="if using http, the request method to use for beaconing. default is get", type=str, choices=['GET', 'POST', 'PUT'], default='GET')  # TODO HEAD?
-parser.add_argument("--start_time", help="if log_only is set, set the start time of the fake simulation (epoch time stamp expected). otherwise the simulation will start at the current time and end in the future", type=int, default=0)
-parser.add_argument("--static_ip", help="a domain might resolve to multiple ips (e.g. when a cdn is used). set this argument to statically log the first observed ip. default is to log a random ip from the set", action="store_true", default=False)
-parser.add_argument("--use_dynamic_urls", help="if using http, use a new randomly generated uri path on each request. default is false", action="store_true", default=False)
-parser.add_argument("--use_round_robin", help="iterate through a number of destinations instead of using just one. switch domains after every x requests or randomly. default is no round robin", type=str, choices=['NONE', 'RANDOM', '1', '5', '10', '50', '100'], default='NONE')
-parser.add_argument("--round_robin_domains", help="comma-separated list of domains, hosts or ips to use for round robin besides the primary one", type=str)
-args   = parser.parse_args()
-beacon = None
-
-
-if args.protocol in ['HTTP', 'HTTPS']:
-    beacon = HttpBeacon(args)
-elif args.protocol == 'DNS':
-    pass  # TODO
-elif args.protocol == 'TCP':
-    pass  # TODO
-elif args.protocol == 'UDP':
-    pass  # TODO
-elif args.protocol == 'SOCKS':
-    beacon = SocksBeacon(args)
-elif args.protocol == 'WEBSOCKET':
-    beacon = WebsocketBeacon(args)
-
-
-
-
 if __name__ == "__main__":
-    beaconing_thread = threading.Thread(target=simulate_beaconing, daemon=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("destination", help="beaconing destination. i.e. the c2 server (fqdn or ip)", type=str)
+    parser.add_argument("interval", help="default beaconing interval (in seconds). default is 30 seconds", type=int, default=30)
+    parser.add_argument("max_requests", help="end the simulation after X requests. default is 720 requests, equating to ~6 hours with a 30 second interval", type=int, default=720)
+    parser.add_argument("--absence", help="make a significant pause of X minutes during the test to simulate the device being offline/sleeping/... default is no absence", type=int, default=0)
+    parser.add_argument("--active_c2_ratio", help="the percentage of requests which should simulate active usage of the c2 channel. i.e. command and result exchange. default is between 0.1 and 3 percent", type=float, default=0.0)
+    parser.add_argument("--data_jitter", help="if log_only is set, add random jitter to the request and response sizes (in percent). default is 11 percent", type=int, default=11)
+    parser.add_argument("--exfil_chunking", help="use chunking when exfiltrating data. i.e. send many small requests with data contained in unique headers or uris instead of one large one (in protocols where applicable). default is to not use chunking", type=str, choices=['NONE', 'HEADER', 'URI'], default='NONE')
+    parser.add_argument("--jitter", help="add random jitter to the time intervals between the beaconing requests (in percent of intervals). default is 17 percent", type=int, default=17)
+    parser.add_argument("--log_only", help="only write log events as they would be expected from the simulation, don't actually dispatch requests. default is to make real requests", action="store_true", default=False)
+    parser.add_argument("--no_c2", help="don't simulate the beacon receiving instructions from the c2 server (i.e. some larger responses, followed by larger requests, followed by temporary slower beaconing). default is to simulate c2 activity", action="store_true", default=False)
+    parser.add_argument("--no_exfil", help="don't simulate the beacon exfiltrating data (similar to c2, but with significantly larger outflow). default is to simulate data exfiltration", action="store_true", default=False)
+    parser.add_argument("--no_noise", help="don't make semi-random, semi-realistic non-beaconing requests in the background to add noise (as user activity would). default is to make background noise", action="store_true", default=False)
+    parser.add_argument("--protocol", help="network protocol to use for beaconing communication. default is http", type=str, choices=['HTTP', 'HTTPS', 'SOCKS', 'WEBSOCKET'], default='HTTP')  # TODO choices=['DNS', 'TCP', 'UDP', '...']
+    parser.add_argument("--reduce_interval_after_c2", help="reduce the polling interval after an active session, simulating how higher stealth could be achieved while the operator works on obtained data", action="store_true", default=False)
+    parser.add_argument("--request_method", help="if using http, the request method to use for beaconing. default is get", type=str, choices=['GET', 'POST', 'PUT'], default='GET')  # TODO HEAD?
+    parser.add_argument("--start_time", help="if log_only is set, set the start time of the fake simulation (epoch time stamp expected). otherwise the simulation will start at the current time and end in the future", type=int, default=0)
+    parser.add_argument("--static_ip", help="a domain might resolve to multiple ips (e.g. when a cdn is used). set this argument to statically log the first observed ip. default is to log a random ip from the set", action="store_true", default=False)
+    parser.add_argument("--use_dynamic_urls", help="if using http, use a new randomly generated uri path on each request. default is false", action="store_true", default=False)
+    parser.add_argument("--use_round_robin", help="iterate through a number of destinations instead of using just one. switch domains after every x requests or randomly. default is no round robin", type=str, choices=['NONE', 'RANDOM', '1', '5', '10', '50', '100'], default='NONE')
+    parser.add_argument("--round_robin_domains", help="comma-separated list of domains, hosts or ips to use for round robin besides the primary one", type=str)
+    args   = parser.parse_args()
+    beacon = None
+
+
+    if args.protocol in ['HTTP', 'HTTPS']:
+        beacon = HttpBeacon(args)
+    elif args.protocol == 'DNS':
+        pass  # TODO
+    elif args.protocol == 'TCP':
+        pass  # TODO
+    elif args.protocol == 'UDP':
+        pass  # TODO
+    elif args.protocol == 'SOCKS':
+        beacon = SocksBeacon(args)
+    elif args.protocol == 'WEBSOCKET':
+        beacon = WebsocketBeacon(args)
+
+    beaconing_thread = threading.Thread(target=simulate_beaconing, args=(beacon,), daemon=True)
     beaconing_thread.start()
 
     if not args.no_noise:
-        noise_thread = threading.Thread(target=make_background_noise, daemon=True)
+        noise_thread = threading.Thread(target=make_background_noise, args=(beacon,), daemon=True)
         noise_thread.start()
 
     # keep the main thread alive
@@ -189,4 +183,5 @@ if __name__ == "__main__":
                 beacon.clean_up()
                 exit(0)
     except KeyboardInterrupt:
+        beacon.clean_up()
         pass
---
+++ mass_simulation.py
@@ -0,0 +1,79 @@
+import os
+import random
+import string
+import sys
+import threading
+import time
+ 
+from pathlib import Path
+ 
+from beaconing_simulation import simulate_beaconing
+from http_beacon          import HttpBeacon
+from socks_beacon         import SocksBeacon
+ 
+ 
+ 
+ 
+NUMBER_OF_HTTP_GET_SIMULATIONS_TO_RUN = 15
+NUMBER_OF_SOCKS_SIMULATIONS_TO_RUN    = 15
+ 
+Path("beaconing_simulation_logs").mkdir(parents=True, exist_ok=True)  # create a logging folder if it doesn't exist in cwd yet
+ 
+ 
+class CustomArgObject():
+    """
+    a substitute for the args from the argparse library that the beacon objects would usually expect for initialization
+    will roll random values for a simulation upon object initialization
+    """
+ 
+    def __init__(self):
+        self.destination: str               = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(8, 20))) + ['.com', '.cn', '.edu', '.io', '.nl'][random.randint(0, 4)]
+        self.interval: int                  = random.randint(10, 1800)
+        self.max_requests: int              = random.randint(100,100)  # 720, 2500
+        self.absence: int                   = 0 if random.randint(0, 100) < 70 else random.randint(4*60, 3*24*60)  # simulate 4h-3d absence in 30% of runs
+        self.active_c2_ratio: float         = random.uniform(0.1, 3.0)
+        self.data_jitter: int               = random.randint(11, 222)
+        self.exfil_chunking: bool           = ['NONE', 'HEADER', 'URI'][random.randint(0, 2)]
+        self.jitter: int                    = random.randint(17, 83)
+        self.log_only: bool                 = True
+        self.no_c2: bool                    = True if random.randint(0, 100) < 15 else False  # don't simulate active c2 usage in only 15% of runs
+        self.no_exfil: bool                 = True if random.randint(0, 100) < 30 else False  # don't simulate data exfiltration in only 30% of runs
+        self.no_noise: bool                 = True  # background noise is not necessary for our purposes
+        self.protocol: str                  = ['HTTP', 'HTTPS', 'SOCKS'][random.randint(0, 2)]
+        self.reduce_interval_after_c2: bool = True if random.randint(0, 100) < 30 else False  # simulate interval reduction after active c2 usage in 30% of runs
+        self.request_method: str            = 'GET' if random.randint(0, 100) < 80 else ['POST', 'PUT'][random.randint(0,1)]  # mostly simulate HTTP GET beacons if HTTP was selected
+        self.start_time: int                = 1753707644 - random.randint(0, 31536000)  # between 2024-07-28 and 2025-07-28
+        self.static_ip: bool                = True if random.randint(0, 100) < 30 else False
+        self.use_dynamic_urls: bool         = True if random.randint(0, 100) < 50 else False
+        self.use_round_robin: str           = 'NONE' if random.randint(0, 100) < 35 else '1' if random.randint(0, 100) < 40 else ['RANDOM', '5', '10', '50', '100'][random.randint(0, 4)]  # no rr in 35% of cases, rr after every request in 26% of cases
+        self.round_robin_domains: str       = ','.join([''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(8, 20))) + ['.com', '.cn', '.edu', '.io', '.nl'][random.randint(0, 4)] for i in range(random.randint(2, 10))])
+ 
+ 
+for i in range(NUMBER_OF_HTTP_GET_SIMULATIONS_TO_RUN + NUMBER_OF_SOCKS_SIMULATIONS_TO_RUN):
+    # TODO make sure we have at least one fast, medium and slow beacon for each type of c2 channel
+    args = CustomArgObject()  # roll random arguments
+ 
+    if i <= NUMBER_OF_HTTP_GET_SIMULATIONS_TO_RUN:
+        beacon = HttpBeacon(args)
+    else:
+        beacon = SocksBeacon(args)
+ 
+    try:
+        simulate_beaconing(beacon)
+    except KeyboardInterrupt:
+        pass
+ 
+    beacon.message_logger.info(f'done. have a nice day :)')
+    beacon.clean_up()
+ 
+    for handler in beacon.message_logger.handlers:
+        handler.close()
+        beacon.message_logger.removeHandler(handler)
+        # TODO somehow the second log handler for writing to file is not destroyed, causing duplicate events to be logged?!
+    for handler in beacon.event_logger.handlers:
+        handler.close()
+        beacon.event_logger.removeHandler(handler)
+ 
+    # move written log files to the new logging folder with good naming
+    os.replace('beaconing_simulation_events.log', f'beaconing_simulation_logs/run{i}_{args.interval}s_{args.max_requests}reqs_{args.protocol}_rr-{args.use_round_robin}_events.log')
+    os.replace('beaconing_simulation_messages.log', f'beaconing_simulation_logs/run{i}_{args.interval}s_{args.max_requests}reqs_{args.protocol}_rr-{args.use_round_robin}_messages.log')
